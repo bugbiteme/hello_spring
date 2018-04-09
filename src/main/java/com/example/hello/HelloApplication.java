cc36277db60c95c86c5b8fe9be14b3b68d7f1f22
Leon Levy <leon.s.levy@gmail.com>
2018-04-09 13:51:19 -0700

    modification based on example at https://github.com/gshipley/bootwildfly

--- src/main/java/com/example/hello/HelloApplication.java
+++ src/main/java/com/example/hello/HelloApplication.java
@@ -5,11 +5,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import javafx.application.Application;
 
 import java.util.Arrays;
 
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
-public class HelloApplication {
+public class HelloApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
@@ -26,5 +35,10 @@ public class HelloApplication {
 				System.out.println(beanName);
 			}
 		};
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
 	}
 }
